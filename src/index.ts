d37e6639a6e53ae9cac6affb03449fe98e7df43c
levisantosp <contatowizard01@gmail.com>
2025-06-08 18:29:24 -0300

    calling port from .env

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import fastify from "fastify"
 import fastifyStatic from "@fastify/static"
 import genCards from "../scripts/genCards.js"
 import path from "path"
+import "dotenv/config"
 
 await genCards()
 fastify()
 .get("/", () => ({ message: "Hello World" }))
 .register(fastifyStatic, { root: path.resolve("output"), prefix: "/cards/" })
 .listen({
-  port: 3000
+  port: process.env.PORT
 })
