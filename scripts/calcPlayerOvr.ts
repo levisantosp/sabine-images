fa4ed8f3ae5b3644ee8454dc343fce754e824c54
levisantosp <contatowizard01@gmail.com>
2025-08-07 01:20:40 -0300

    changed the calculation method

--- scripts/calcPlayerOvr.ts
+++ scripts/calcPlayerOvr.ts
@@ -1,5 +1,5 @@
 import type { Player } from './getPlayers.ts'
 
 export default function(player: Player) {
-  return (player.aim + player.HS + player.movement + player.aggression + player.ACS + player.gamesense) / 4.5
+  return (player.aim + player.HS + player.movement + player.aggression + player.ACS + player.gamesense) / 6
 }
