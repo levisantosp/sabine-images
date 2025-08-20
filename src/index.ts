e9663a614735cb1e4c512ce9a1600eaae5037811
levisantosp <contatowizard01@gmail.com>
2025-08-20 16:07:01 -0300

    fix Strings must use doublequote

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import fastify from 'fastify'
-import fastifyStatic from '@fastify/static'
-import genCards from '../scripts/genCards.ts'
-import path from 'path'
+import fastify from "fastify"
+import fastifyStatic from "@fastify/static"
+import genCards from "../scripts/genCards.ts"
+import path from "path"
 
 await genCards()
 fastify()
-.get('/', () => ({ message: 'Hello World' }))
-.register(fastifyStatic, { root: path.resolve('output'), prefix: '/cards/' })
+.get("/", () => ({ message: "Hello World" }))
+.register(fastifyStatic, { root: path.resolve("output"), prefix: "/cards/" })
 .listen({
   port: process.env.PORT,
   host: process.env.HOST
