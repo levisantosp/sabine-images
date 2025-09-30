d4725581d45196048ba92a651048944ce1c64295
levisantosp <contatowizard01@gmail.com>
2025-09-30 17:07:00 -0300

    fix champs & masters cards

--- src/config.ts
+++ src/config.ts
@@ -79,6 +79,86 @@ export const config = {
           left: 65
         }
       }
+    },
+    masters: {
+      role: {
+        left: 0,
+        top: 0
+      },
+      country: {
+        top: 214,
+        left: 135
+      },
+      team: {
+        top: 360,
+        left: 120
+      },
+      stats: {
+        aim: {
+          top: 120,
+          left: -65
+        },
+        hs: {
+          top: 190,
+          left: -65
+        },
+        movement: {
+          top: 260,
+          left: -65
+        },
+        aggression: {
+          top: 120,
+          left: 65
+        },
+        acs: {
+          top: 190,
+          left: 65
+        },
+        gamesense: {
+          top: 260,
+          left: 65
+        }
+      }
+    },
+    champions: {
+      role: {
+        left: 0,
+        top: 0
+      },
+      country: {
+        top: 214,
+        left: 135
+      },
+      team: {
+        top: 360,
+        left: 120
+      },
+      stats: {
+        aim: {
+          top: 120,
+          left: -65
+        },
+        hs: {
+          top: 190,
+          left: -65
+        },
+        movement: {
+          top: 260,
+          left: -65
+        },
+        aggression: {
+          top: 120,
+          left: 65
+        },
+        acs: {
+          top: 190,
+          left: 65
+        },
+        gamesense: {
+          top: 260,
+          left: 65
+        }
+      }
     }
   },
   ovr: {
@@ -89,6 +169,14 @@ export const config = {
     'triple crown': {
       left: -215,
       top: -250
+    },
+    masters: {
+      left: -215,
+      top: -280
+    },
+    champions: {
+      left: -215,
+      top: -280
     }
   },
   stats: {
@@ -119,6 +207,58 @@ export const config = {
       }
     },
     'triple crown': {
+      aim: {
+        left: -210,
+        top: 120
+      },
+      hs: {
+        left: -180,
+        top: 190
+      },
+      movement: {
+        left: -210,
+        top: 260
+      },
+      aggression: {
+        left: 120,
+        top: 120
+      },
+      acs: {
+        left: 120,
+        top: 190
+      },
+      gamesense: {
+        left: 120,
+        top: 260
+      }
+    },
+    masters: {
+      aim: {
+        left: -210,
+        top: 120
+      },
+      hs: {
+        left: -180,
+        top: 190
+      },
+      movement: {
+        left: -210,
+        top: 260
+      },
+      aggression: {
+        left: 120,
+        top: 120
+      },
+      acs: {
+        left: 120,
+        top: 190
+      },
+      gamesense: {
+        left: 120,
+        top: 260
+      }
+    },
+    champions: {
       aim: {
         left: -210,
         top: 120
