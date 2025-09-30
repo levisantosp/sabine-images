2e61733789cbc89a4acd7b5dfa6d55bc487776be
levisantosp <contatowizard01@gmail.com>
2025-09-30 16:09:57 -0300

    fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import fastify from "fastify"
-import getCard from "../scripts/getCard.ts"
+import fastify from 'fastify'
+import getCard from './scripts/getCard.ts'
 
 fastify()
-.get("/", () => ({ message: "Hello World" }))
-.register(getCard)
-.listen({
-  port: process.env.PORT,
-  host: process.env.HOST
-})
+  .get('/', () => ({ message: 'Hello World' }))
+  .register(getCard)
+  .listen({
+    port: process.env.PORT,
+    host: process.env.HOST
+  })
